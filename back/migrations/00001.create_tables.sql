1c6bb60862a146431b5935e3314b10a79f277f7f
sangeerang <sangeetha.ranganathan@hellofresh.com.au>
2022-05-12 12:48:31 +1000

    removed new table creation scripts

--- back/migrations/00001.create_tables.sql
+++ back/migrations/00001.create_tables.sql
@@ -11,20 +11,5 @@ create table customer_order (
     customer_swaps  varchar
 );
 
-create table substitute_options (
-    order_id varchar references customer_order,
-    recipe_name varchar ,
-    swap_name varchar,
-    actual_ingredient VARCHAR,
-    swap_options VARCHAR[]
-);
 
-create table customer_substitutions (
-    order_id varchar references customer_order,
-    recipe_name varchar ,
-    actual_ingredient varchar,
-    substitute_ingredient varchar,
-    order_status varchar
-
-);
 
