fd4bd238ab5d3761fa24aadd6dead3808fba9ba3
sagafonoff <sandor.agafonoff@hellofresh.com.au>
2022-05-12 13:36:21 +1000

    initial customer swap api

--- front/src/app/feature/home/HomeView.tsx
+++ front/src/app/feature/home/HomeView.tsx
@@ -1,20 +1,32 @@
 import { useApiContext } from "@app/core/api/ApiContext";
 import { useEffect, useState } from "react";
 
+export type CustomerRecipeSwap = {
+  orderId: string;
+  customerId: string;
+  customerName: string;
+  recipe: string;
+  swap: string;
+  ingredient: string;
+  availability: string[];
+};
+export type CustomerSwapList = CustomerRecipeSwap[];
+
 export function HomeView() {
   const api = useApiContext();
-  const [apiResponse, setApiResponse] = useState<string>("PENDING");
-
+  const [customerSwapList, setCustomerSwapList] = useState<CustomerSwapList>(
+    []
+  );
+  const orderId = "AU112233445";
   useEffect(() => {
     api
-      ?.apiRequest<Record<string, unknown>, string>(`helloWorld`, {})
+      ?.apiRequest<{ orderId: string }, CustomerSwapList>(`getSkuSubOptions`, {
+        orderId,
+      })
       .then((res) => {
-        const msg = res?.data;
-        let status = "failed";
-        if (msg) {
-          status = msg;
+        if (res.data) {
+          setCustomerSwapList(res.data);
         }
-        setApiResponse(status.toUpperCase());
       })
       .catch((error) => {
         console.error(error);
@@ -23,7 +35,10 @@ export function HomeView() {
   return (
     <div>
       <h1>Welcome to the SKU Substitution Service</h1>
-      <h3>The backend status is: {apiResponse}</h3>
+      <h3>
+        The customer swap list is:{" "}
+        {customerSwapList.length ? customerSwapList : `EMPTY`}
+      </h3>
     </div>
   );
 }
--- front/src/services/http/api/sku-sub-options/api-requests.ts
+++ front/src/services/http/api/sku-sub-options/api-requests.ts
@@ -1,6 +1,17 @@
 import { ApiResponse, SkuSubRequest } from "@app/core/api/backend";
+import { CustomerSwapList } from "@app/feature/home/HomeView";
 import { HttpResponse } from "@services/http/adapters/http-adapter";
 import { PartialRequestConfig } from "@services/http/http-request-map";
+
+type GetOrderResponse = {
+  order_id: string;
+  customer_id: string;
+  customer_name: string;
+  recipe_name: string;
+  swap_name: string;
+  actual_ingredient: string;
+  options_available: string[];
+};
 
 export type SkuSubRequestMap = () => Record<
   SkuSubRequest,
@@ -21,12 +32,21 @@ export const skuSubApiRequests: SkuSubRequestMap = () => ({
     method: "GET",
     path: ["orderId"],
     transformResponse: (
-      response: HttpResponse<{ orderId: string; customerId: string }>
-    ): ApiResponse<{ orderId: string; customerId: string }> => ({
-      data: {
-        orderId: response.data.orderId,
-        customerId: response.data.customerId,
-      },
-    }),
+      response: HttpResponse<GetOrderResponse[]>
+    ): ApiResponse<CustomerSwapList> => {
+      return {
+        data: response.data.map((r) => {
+          return {
+            orderId: r.order_id,
+            customerId: r.customer_id,
+            customerName: r.customer_name,
+            recipe: r.recipe_name,
+            swap: r.swap_name,
+            ingredient: r.actual_ingredient,
+            availability: r.options_available,
+          };
+        }),
+      };
+    },
   },
 });
